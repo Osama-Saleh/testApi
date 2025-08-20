d6b4d00e63c758d555dc1498e6e6377201312cf2
Osama-Saleh <osamasaleh509@gmail.com>
2025-08-20 21:16:40 +0300

    handle api

--- lib/core/models/product_model/product_model.dart
+++ lib/core/models/product_model/product_model.dart
@@ -12,7 +12,7 @@ class ProductModel {
     required this.description,
     required this.image,
   });
-
+  //* name constructor
   factory ProductModel.fromJson(Map<String, dynamic> json) {
     return ProductModel(
       idProduct: json['id'],
